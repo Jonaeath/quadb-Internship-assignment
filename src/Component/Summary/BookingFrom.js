d441fec8ca8f8824e983590c705ee4a0f972f8a7
Jonaeath <jonaeathbcc18@gmail.com>
2024-02-01 20:32:55 +0600

    Feat: booking from

---
+++ src/Component/Summary/BookingFrom.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const BookingFrom = ({ movieName, onClose }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onClose();
+  };
+
+  return (
+    <div>
+      <div
+        style={{
+          border: "1px solid #0dcaf0",
+          padding: "10px",
+          borderRadius: "8px",
+          marginTop: "20px",
+        }}
+      >
+        <h3>Book a Movie Ticket</h3>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Movie Name:
+            <input type="text" value={movieName} readOnly />
+          </label>
+          <br />
+          <button
+            type="submit"
+            style={{
+              backgroundColor: "#0dcaf0",
+              borderColor: "#0dcaf0",
+              color: "black",
+            }}
+          >
+            Booking
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default BookingFrom;
--- src/Component/Summary/Summary.js
+++ src/Component/Summary/Summary.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import BookingFrom from "./BookingFrom";
 
 const SquareBox = {
   border: "1px solid #0dcaf0",
@@ -13,6 +15,7 @@ const Summary = () => {
   const { id } = useParams();
 
   const [showData, setShowData] = useState(null);
+  const [mBookingFrom, setBookingForm] = useState(false);
 
   useEffect(() => {
     const url = `https://api.tvmaze.com/shows/${id}`;
@@ -29,10 +32,33 @@ const Summary = () => {
 
   const { name, summary } = showData;
 
+  const handleBookTicket = () => {
+    setBookingForm(true);
+  };
+
+  const handleCloseForm = () => {
+    setBookingForm(false);
+  };
+
   return (
     <div style={{ ...SquareBox }}>
-      <h2>{name}</h2>
+      <h2>Movie Nmae: {name}</h2>
       <div dangerouslySetInnerHTML={{ __html: summary }} />
+
+      <Button
+        style={{
+          backgroundColor: "#0dcaf0",
+          borderColor: "#0dcaf0",
+          color: "black",
+        }}
+        onClick={handleBookTicket}
+      >
+        Book a Movie Ticket
+      </Button>
+
+      {mBookingFrom && (
+        <BookingFrom movieName={name} onClose={handleCloseForm} />
+      )}
     </div>
   );
 };
